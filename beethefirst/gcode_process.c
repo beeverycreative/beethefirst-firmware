a1817b553db0f788e8028748a70a99b54a1172b5
Ivo Angélico <ivoangelico@ua.pt>
2014-07-16 11:21:09 +0100

    4.32.1

--- beethefirst/gcode_process.c
+++ beethefirst/gcode_process.c
@@ -990,7 +990,7 @@ eParseResult process_gcode_command(){
           {
             if(!next_target.seen_B && !sd_printing){
 
-                serial_writestr(" 3.32.1");
+                serial_writestr(" 4.32.1");
                 serial_writestr(" ");
             }
           }
