c7f753325ed74ca423168eec6650ac766d309f23
Ivo Angélico <mgomes@beeverycreative.com>
2015-04-23 11:25:12 +0100

    Removed Config ID from current release. It is present in 8.0.0

--- beethefirst/config.h
+++ beethefirst/config.h
@@ -36,12 +36,8 @@
 #define MM_REPRAP_MENDEL  0
 #define MM_RAPMAN         1
 
-#define CFG_UID           0x10000000
-
 struct configuration
 {
-  int32_t uid;
-
   int32_t  machine_model;
   
   double steps_per_mm_x;
--- libraries/R2C2/usb.c
+++ libraries/R2C2/usb.c
@@ -66,8 +66,10 @@ static U8 abDescriptors[] = {
       0x00,                       // bDeviceSubClass
       0x00,                       // bDeviceProtocol
       MAX_PACKET_SIZE0,           // bMaxPacketSize
-      LE_WORD(0xFFFF),            // idVendor
-      LE_WORD(0x014e),               // idProduct
+      //LE_WORD(0x29C9),            // idVendor bootloader 4.x.x
+      //LE_WORD(0x001),               // idProduct bootloader 4.x.x
+      LE_WORD(0xffff),            // idVendor bootloader 3.x.x
+      LE_WORD(0x014e),               // idProduct bootloader 3.x.x
       LE_WORD(0x0100),            // bcdDevice
       0x01,                       // iManufacturer
       0x02,                       // iProduct
