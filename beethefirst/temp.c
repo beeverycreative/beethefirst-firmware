915c1968254bc996f756d179b7d73dec77553c8a
marcosfg@ua.pt <marcosfg@ua.pt>
2016-01-08 18:29:49 +0000

    Heated Chamber Bug

--- beethefirst/temp.c
+++ beethefirst/temp.c
@@ -244,6 +244,10 @@ void temp_tick(void)
     {
       digital_write(CHAMBER_HEATER_PORT, CHAMBER_HEATER_PIN, 1);
     }
+  else
+    {
+      digital_write(CHAMBER_HEATER_PORT, CHAMBER_HEATER_PIN, 0);
+    }
 #if defined(BTF_SMOOTHIE) && !defined(BTF_SMOOTHIE_V1) && defined(BTF_SMOOTHIE_V2)
 
   if((current_temp[CHAMBER] > target_temp[CHAMBER] + 5) && target_temp[CHAMBER] != 0)
