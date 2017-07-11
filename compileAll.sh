af06cb441636de4c50db2e8484c650c8be99d269
Marcos Gomes <marcosfg@ua.pt>
2017-07-11 11:19:01 +0100

    Removed BEEME from compatible printers

--- compileAll.sh
+++ compileAll.sh
@@ -5,7 +5,6 @@ export PATH="$PATH:/Users/mgomes/Toolchains/gcc-arm-none-eabi_4.8/bin/"
 make clean btf
 make clean btfold
 make clean btfplus
-make clean btfme
 make clean btfschool
 make clean btfplus_a
 make clean btfschool_a
